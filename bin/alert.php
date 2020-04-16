7eb22cb0d2b34cedaf99daa797f7a9470cf840f9
Eli Silakov <me@ileamare.ru>
2020-04-16 19:59:52 +0300

    alert.php

---
+++ bin/alert.php
@@ -0,0 +1,38 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$config = \json_decode(\file_get_contents("config.json"), true);
+// -d body="$fname" -d title="Ranked Fetched"
+
+$argline = implode(' ', array_slice($argv, 1));
+
+if (strpos($argline, '-d ') !== false) {
+  $args = explode('-d ', $argline);
+  $data = "";
+
+  foreach ($args as $a) {
+    if (empty($a)) continue;
+    $arg = explode('=', $a);
+    $arg[1] = trim($arg[1], " \"");
+    if ($arg[0] === 'title')
+      $data = "\> _*".addcslashes($arg[1], "_*[]()~`>#+-=|{}.!\\")."*_\n".$data;
+    else
+      $data = $data."\n".addcslashes($arg[1], "_*[]()~`>#+-=|{}.!\\");
+  }
+} else {
+  $data = $argline;
+}
+
+$loop = React\EventLoop\Factory::create();
+$connector = new React\Socket\Connector($loop);
+
+$connector->connect($config['server'])->then(function (React\Socket\ConnectionInterface $connection) use ($data) {
+    $connection->write($data."\n");
+    $connection->end();
+});
+
+$loop->run();
